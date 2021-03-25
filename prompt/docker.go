17034f6258d89f2b0a5e446ed5a5e2c13154463e
edwinvautier <edwin.vautier@hetic.net>
2021-03-25 17:35:03 +0100

    refacto(cli): Add comment to exported function AskToUseDocker.

--- prompt/docker.go
+++ prompt/docker.go
@@ -4,6 +4,7 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 )
 
+// AskToUseDocker simply asks the user if the app should be dockerized or not
 func AskToUseDocker(wantsDocker *bool) error {
 	prompt := &survey.Confirm{
     Message: "Do you want to use docker ?",
