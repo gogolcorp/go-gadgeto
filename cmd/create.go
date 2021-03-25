43811c8944aa8ba5239b4dc04fe4271ec655b61c
edwinvautier <edwin.vautier@hetic.net>
2021-03-25 18:00:52 +0100

    refacto(go): Reduce complexity of the create.go file.

--- cmd/create.go
+++ cmd/create.go
@@ -9,8 +9,6 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/AlecAivazis/survey/v2"
-	//"github.com/edwinvautier/go-project-cli/services"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/spf13/cobra"
@@ -22,41 +20,10 @@ var createCmd = &cobra.Command{
 	Short: "This command is used to initialize a new application.",
 	Long:  `This command is used to initialize a new application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		appName := strings.Join(args, "-")
-
-		// Check if the app name is empty
-		if appName == "" {
-			if err := prompt.AskApplicationName(&appName); err != nil {
-				log.Error(err)
-				return
-			}
-
-			appName = helpers.JoinString(appName)
+		config := CreateCmdConfig{
+			Args: args,
 		}
-
-		// Get git username
-		userName := services.GetGitUsername()
-		if userName == "" {
-			if err := prompt.AskGitUsername(&userName); err != nil {
-				log.Fatal(err)
-			}
-			viper.Set("git-username", userName)
-			if err := viper.WriteConfig(); err != nil {
-				log.Error(err)
-			}
-		}
-
-		// Get the desired DB management system
-		dbms := ""
-		if err := prompt.AskDBMS(&dbms); err != nil {
-			log.Fatal(err)
-		}
-
-		// Ask user wether to use docker or not
-		wantsDocker := false
-		if err := prompt.AskToUseDocker(&wantsDocker); err != nil {
-			log.Fatal(err)
-		}
+		initCmdConfig(&config)
 
 		/*
 		path, err := os.Getwd()
@@ -78,24 +45,59 @@ func init() {
 		log.Info("Exiting...")
 		os.Exit(1)
 	}()
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func promptUserForModules() []string {
-	modules := []string{}
-	prompt := &survey.MultiSelect{
-		Message: "What modules do you want:",
-		Options: []string{"Router", "Database", "Docker"},
-	}
-	survey.AskOne(prompt, &modules)
+func initCmdConfig(config *CreateCmdConfig) {
+	config.AppName = getAppName(config.Args)
+	config.GitUserName = getGitUsername()
+	config.DBMS = getDBMS()
+	config.UseDocker = chooseToUseDocker()
+}
 
-	return modules
+func getAppName(args []string) string {
+	appName := strings.Join(args, "-")
+
+	// Check if the app name is empty
+	if appName == "" {
+		prompt.AskApplicationName(&appName)
+		appName = helpers.JoinString(appName)
+	}
+	return appName
+}
+
+func getGitUsername() string {
+	userName := services.GetGitUsername()
+	if userName == "" {
+		if err := prompt.AskGitUsername(&userName); err != nil {
+			log.Fatal(err)
+		}
+		viper.Set("git-username", userName)
+	}
+	return userName
+}
+
+func getDBMS() string {
+	// Get the desired DB management system
+	dbms := ""
+	if err := prompt.AskDBMS(&dbms); err != nil {
+		log.Fatal(err)
+	}
+	return dbms
+}
+
+func chooseToUseDocker() bool {
+	// Ask user wether to use docker or not
+	wantsDocker := false
+	prompt.AskToUseDocker(&wantsDocker)
+
+	return wantsDocker
+}
+
+type CreateCmdConfig struct {
+	AppName 		string
+	GitUserName string
+	DBMS				string
+	UseDocker		bool
+
+	Args				[]string
 }
