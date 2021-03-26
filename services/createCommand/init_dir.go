75412ea7947fef6823c21128e25d3ca0ae235e1f
edwinvautier <edwin.vautier@hetic.net>
2021-03-26 14:10:06 +0100

    feat(createcmd): A directory is now created.

--- services/createCommand/init_dir.go
+++ services/createCommand/init_dir.go
@@ -1,17 +1,24 @@
 package createCommand
 
 import (
+	"errors"
 	"os"
 
 	"github.com/edwinvautier/go-cli/config"
+	"github.com/edwinvautier/go-cli/prompt"
 	log "github.com/sirupsen/logrus"
 )
 
 // InitProject creates the directory for a new project and all needed structure depending on the config given
 func InitProject(config *config.CreateCmdConfig) error {
 	workingDirectory := getWorkDir()
-	log.Info(workingDirectory)
+	projectPath := workingDirectory + "/" + config.AppName
 
+	if err := createProjectDir(projectPath); err != nil {
+		return err
+	}
+	log.WithField("path", projectPath).Info("project directory created")
+	
 	return nil
 }
 
@@ -22,4 +29,27 @@ func getWorkDir() string {
 	}
 
 	return path
+}
+
+func createProjectDir(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return os.Mkdir(path, os.ModePerm)
+	}
+	log.Warn("A directory with this name already exists.")
+
+	wantsOverride := false
+	prompt.AskToOverride(&wantsOverride)
+
+	if wantsOverride {
+		return removeAll(path)
+	}
+
+	return errors.New("Couldn't create project directory")
+}
+
+func removeAll(path string) error {
+	if err := os.RemoveAll(path); err != nil {
+		return err
+	}
+	return os.Mkdir(path, os.ModePerm)
 }
