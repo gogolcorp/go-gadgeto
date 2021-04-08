ccf7b3e297854b8174d383f0c14dab574ed37181
edwinvautier <edwin.vautier@hetic.net>
2021-04-08 15:30:24 +0200

    fix: Fixed comments

--- cmd/make.go
+++ cmd/make.go
@@ -1,7 +1,7 @@
 package cmd
 
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 Edwin Vautier <edwin.vautier@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
