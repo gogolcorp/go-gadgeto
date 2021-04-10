3a32c39126e78e50b549794ab3804c1e44d35094
edwinvautier <edwin.vautier@hetic.net>
2021-04-10 15:58:40 +0200

    fix: fix codacy issue

--- bundles/authenticator/install.sh
+++ bundles/authenticator/install.sh
@@ -3,4 +3,4 @@
 go get -u github.com/edwinvautier/go-cli
 go mod tidy
 
-echo "## authenticator-bundle\n# token duration (minutes)\nTOKEN_VALID_DURATION=20\nRSA_PUBLIC_PATH=public.pem\nRSA_PRIVATE_PATH=private.pem\nRSA_PASSWORD=YOURPASSWORDHERE\nDOMAIN=localhost\n## authenticator-bundle end" >> .env.dist
+printf "## authenticator-bundle\n# token duration (minutes)\nTOKEN_VALID_DURATION=20\nRSA_PUBLIC_PATH=public.pem\nRSA_PRIVATE_PATH=private.pem\nRSA_PASSWORD=YOURPASSWORDHERE\nDOMAIN=localhost\n## authenticator-bundle end" >> .env.dist
